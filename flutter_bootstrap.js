ab9bc2344cdab11d8de519ff7936960dab683e7b
Reginald Appiah <reginald.appiah7@gmail.com>
2025-07-09 23:41:30 -0400

    [VODS ATL] 🚀 Deploy 2025-07-09 23:41:30 Update: minor changes in AppRegin Suite

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3457070523"
+    serviceWorkerVersion: "141768813"
   }
 });
